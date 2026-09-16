72b625c5aefd3524b513a3bf9bc11ba678e66762
RonFer87 <ronald.ricaldi@gmail.com>
2026-09-16 08:07:31 -0400

    primera actualizacion

---
+++ EJERCICIO_1.py
@@ -0,0 +1,25 @@
+
+n = int(input("Ingrese un numero: "))
+
+while n <= 0:
+    print("Debe ingresar un numero mayor a 0")
+    n = int(input("Ingrese un numero entero positivo: "))
+
+a = 0
+b = 1
+contador = 0
+
+print(f"Los primeros {n} terminos son:")
+
+
+while contador < n:
+    
+    if contador < n - 1:
+        print(a, end=", ")
+    else:
+        print(a)
+    
+    c = a + b
+    a = b
+    b = c
+    contador = contador + 1 
